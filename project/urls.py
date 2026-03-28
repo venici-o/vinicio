dedacbbb43a01243d2d86abe5fe16a420b4c0d0e
euclides <joaomartires2@gmail.com>
2026-03-28 15:07:48 -0300

    refactor: use of django forms was not allowed in the project

--- project/urls.py
+++ project/urls.py
@@ -1,15 +1,8 @@
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path(
-        "accounts/login/",
-        auth_views.LoginView.as_view(template_name="registration/login.html"),
-        name="login",
-    ),
-    path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("accounts/", include("apps.accounts.urls")),
     path("transactions/", include("apps.transactions.urls")),
     path("categories/", include("apps.categories.urls")),
