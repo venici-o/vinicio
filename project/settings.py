1faf214ac7185c8477cb267210f8a91b68fe9748
euclides <joaomartires2@gmail.com>
2026-03-30 17:26:45 -0300

    fix: add CSRF_TRUSTED_ORIGINS for azure

--- project/settings.py
+++ project/settings.py
@@ -20,6 +20,10 @@ DEBUG = os.environ.get("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
+CSRF_TRUSTED_ORIGINS = os.environ.get(
+    "CSRF_TRUSTED_ORIGINS", "http://localhost,http://127.0.0.1"
+).split(",")
+
 
 # Application definition
 
