bdaaf40c3d08907fd4a9c6d238471bcb224324a7
RiosGabri <grp@cesar.school>
2026-04-19 19:36:48 -0300

    feat: add category field and choices to Transaction model

--- apps/transactions/models.py
+++ apps/transactions/models.py
@@ -7,7 +7,18 @@ class Transaction(models.Model):
         ('DEPOSIT', 'Entrada'),
         ('WITHDRAWAL', 'Saída'),
     ]
-    # Campos de uma transação (provisórios)
+
+    CATEGORY_CHOICES = [
+        ('FOOD', 'Alimentação'),
+        ('TRANSPORT', 'Transporte'),
+        ('ENTERTAINMENT', 'Lazer'),
+        ('BILLS', 'Contas'),
+        ('SAVINGS', 'Poupança'),
+        ('HEALTH', 'Saúde'),
+        ('EDUCATION', 'Educação'),
+        ('OTHER', 'Outros'),
+    ]
+    
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=150, verbose_name="Descrição")
     value = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Valor")
@@ -15,6 +26,12 @@ class Transaction(models.Model):
         max_length=10, 
         choices=TYPE_CHOICES, 
         verbose_name="Tipo de Transação"
+    )
+    category = models.CharField(
+        max_lenght = 20,
+        choices=CATEGORY_CHOICES,
+        default = 'OTHER',
+        verbose_name = "Categoria"
     ) 
     date = models.DateField(auto_now_add=True, verbose_name="Data") 
 
