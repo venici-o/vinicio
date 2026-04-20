f47c23631a41a1b36b0c04d5e31f600fbb093cf1
LarissaGiovanna <lgiova544@gmail.com>
2026-04-20 12:07:34 -0300

    feat: function add category in transactions page - html and js changed to sent a category in a new transaction

--- static/js/transactions/create_transaction.js
+++ static/js/transactions/create_transaction.js
@@ -15,3 +15,45 @@
         input.value = input.value.replace(/\./g, '');
     });
 }());
+
+const modal = document.getElementById('category-modal');
+const pageTitle = document.querySelector('h2');
+
+function openCategoryModal() {
+    if (modal) {
+        modal.style.display = 'block';
+    }
+
+    if (pageTitle) {
+        pageTitle.textContent = 'Selecionar Categoria';
+    }
+
+    const backBtn = document.getElementById('back-btn');
+    if (backBtn) {
+        backBtn.onclick = function () {
+            modal.style.display = 'none';
+            console.log('botao clicado')
+            pageTitle.textContent = 'Criar Transação';
+            console.log(pageTitle.textContent)
+        };
+    }
+
+}
+
+function selectCategory(categoryValue, categoryLabel) {
+    if (modal) {
+        modal.style.display = 'none';
+        pageTitle.textContent = 'Criar Transação';
+    }
+
+    const hiddenCategoryInput = document.getElementById('selected-category');
+    if (hiddenCategoryInput) {
+        hiddenCategoryInput.value = categoryValue;
+    }
+
+    const categoryBtn = document.getElementById('category-btn');
+    if (categoryBtn) {
+        categoryBtn.innerHTML = `<p>${categoryLabel}</p>`;
+    }
+}
+
