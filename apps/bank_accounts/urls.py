b91609cd3012b7696b4128405d5201eab1377485
Torzinus <hcs4@cesar.school>
2026-04-26 17:31:02 -0300

    adiciona csszinho

--- apps/bank_accounts/urls.py
+++ apps/bank_accounts/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.accounts_list, name = 'my_accounts'),
+    path('', views.accounts_list, name='bank_accounts'),
+    path('register/', views.register_account, name='register_account'),
+    path('register/manual/', views.register_manual, name='register_manual'),
+    path('register/auto/', views.register_auto, name='register_auto'),
 ]
+
+#urls: .../my_accounts/
+#.../my_accounts/register/
