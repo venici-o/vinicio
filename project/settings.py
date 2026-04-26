7aae4d143d4082ffae65537e1686fe3752b418fc
Euclides <joaomartires2@gmail.com>
2026-04-26 13:13:43 -0300

    fix: date/time formats automatically, no need for months array

--- project/settings.py
+++ project/settings.py
@@ -137,7 +137,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "pt-br"
 
 TIME_ZONE = "UTC"
 
