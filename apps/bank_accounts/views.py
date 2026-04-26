cd4d4446983f9c2009387f4d8cd66a87692b3aed
Torzinus <hcs4@cesar.school>
2026-04-26 17:38:33 -0300

    fixed minor errors

--- apps/bank_accounts/views.py
+++ apps/bank_accounts/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth.decorators import login_required
 from .models import Conta
 @login_required
 def accounts_list(request):
-    contas = Conta.objects.all()
     return render(request, 'bank_accounts/myaccounts.html')
 
 #tela para adicionar conta
